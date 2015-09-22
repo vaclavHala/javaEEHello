a386cb6099464027491bc876973f32e5cc0fd92c
Vaclav Hala <xhala3@nymfe40.fi.muni.cz>
2015-09-22 16:35:18 +0200

    + initial hello

---
+++ src/main/java/org/toy/App.java
@@ -0,0 +1,13 @@
+package org.toy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
